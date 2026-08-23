062aadb4c4c3905782e33e9b726ec9b9ce4ba304
Antonio <a.caristia@gmail.com>
2026-08-23 14:59:53 +0200

    Add starter for first Python Activity

---
+++ activities/python/py2-activity-b-input-somma-001/starter/main.py
@@ -0,0 +1,9 @@
+# Leggi due numeri interi, uno per riga.
+primo = int(input())
+secondo = int(input())
+
+# TODO: sostituisci il valore qui sotto con il calcolo corretto.
+risultato = 0
+
+# Non aggiungere altri messaggi: stampa soltanto il risultato.
+print(risultato)
